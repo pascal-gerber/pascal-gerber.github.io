872e75aa30160b156d70dec9ebea3c3184f8d842
Noodle doodle <77777914+pascal-gerber@users.noreply.github.com>
2022-07-14 11:19:15 +0200

    Lists and Objects
    :b

---
+++ portfolio/Javascript background/javascript arrays.js
@@ -0,0 +1,30 @@
+//lists have a simple syntax.
+//Thier syntax matches pythons syntax
+//lists here can contain any value.
+var peoples = ["David", "Rebecca", "Joe", "Mark", "Philip"];
+
+//this is a list of strings
+var David = ["David", 34, 1.8, true]
+//A list can contain strings, integers, doubles(floats) or Booleans
+
+//a list starts with the index 0, so you have to think shifted
+console.log(peoples[1], "index 1 of list")
+
+//list part
+///////////////////////////////////////////////////////////////////////////////
+
+
+//theres also what we call "objects" in javascript, known in python as "dictionnaries"
+
+var objects = {Name: "David", Age: 34, Height: 1.8, knowsAboutAllThis: true};
+
+console.log("Name:", objects.Name)
+console.log("Age:", objects.Age)
+console.log("Height:", objects.Height, "m")
+console.log("Knows JS:", objects.knowsAboutAllThis)
+
+//made by pascal
+//enjoy
+
+//PS: No david, you are not an object :b
+
