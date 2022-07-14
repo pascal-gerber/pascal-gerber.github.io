83cd2383d4421b66e416e71f7060d73ee88741d5
Noodle doodle <77777914+pascal-gerber@users.noreply.github.com>
2022-07-14 10:07:57 +0200

    Javascript
    Start of javascript mini library

---
+++ portfolio/Javascript background/Variables.js
@@ -0,0 +1,40 @@
+//Let standing for Letter is an old way to write a variable type
+//some old functions are supported on it, but usually
+//not used today anymore
+let oldVariable = 20
+//Still works perfectly
+
+
+//Var standing for Variable is the newer version of a variable
+//var has more recent function possibilities
+var newVariable = 20
+
+//const meaning Constant is here to signal a value that cannot be changed
+const unchangable = 10
+
+
+//new variables can be made on the flight
+//while all the variable types can interact with each other
+
+var allThree = oldVariable + newVariable + unchangable
+
+
+//those commands can be used for printing out a result
+//tho console.log can be seen on your browsers console.
+//by Pressing F12 or "Ctrl + Shift + i" you open the viewer
+//then select the console
+
+//alert doesn't require a console, it will simply pop up on the top
+alert(allThree)
+//or
+console.log(allThree)
+//theres another which concists of writting in a tag
+
+//this line
+//document.getElementById("P-tag").innerHTML = "New Value";
+
+//The tag must contain the "id="P-tag" in order to work"
+
+
+//Made by pascal
+//:D
