379d5928b9452b400dd0627b2833724e3f2894fd
Noodle doodle <77777914+pascal-gerber@users.noreply.github.com>
2022-07-18 16:00:31 +0200

    Add files via upload

--- games/Gues the number/Logic.js
+++ games/Gues the number/Logic.js
@@ -1,4 +1,4 @@
-let SelectedRandom = 0
+let SelectedRandom = Math.floor(Math.random * 100)
 
 function GenerateNumb(){
     SelectedRandom = Math.floor(Math.random * 100)
@@ -11,22 +11,18 @@ function Checkifnumber() {
         
         let userInput = document.getElementById("UserText").value
         
+        document.getElementById("makesNewText").innerHTML = SelectedRandom.toString();
+        
         if(Number(userInput) > SelectedRandom){
             document.getElementById("makesNewText").innerHTML = "Too big";
-        }
-        else if(Number(userInput) < SelectedRandom){
+        } else if(Number(userInput) < SelectedRandom){
             document.getElementById("makesNewText").innerHTML = "Too small";
-        }
-    
-        else if(Number(userInput) == SelectedRandom){
+        } else if(Number(userInput) == SelectedRandom){
             document.getElementById("makesNewText").innerHTML = "you guessed it, it was " + userInput.toString();
-        }
-        
-        else{
-           document.getElementById("makesNewText").innerHTML = "Do you even know what a number is?";
+        } else {
+           //document.getElementById("makesNewText").innerHTML = "Do you even know what a number is?";
         }
         
     }
 
 
-GenerateNumb()
