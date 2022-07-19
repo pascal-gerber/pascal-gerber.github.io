ee6d3353c9cf70fdeb6b3b4c06aae47b674cd14c
Noodle doodle <77777914+pascal-gerber@users.noreply.github.com>
2022-07-19 09:31:55 +0200

    working site
    gg did it

--- games/Gues the number/Logic.js
+++ games/Gues the number/Logic.js
@@ -1,16 +1,17 @@
-let SelectedRandom = Math.floor(Math.random * 100)
+let SelectedRandom = Math.floor(Math.random() * 100)
+let numberOfTries = 0
 
+//for restarting the game
 function GenerateNumb(){
-    SelectedRandom = Math.floor(Math.random * 100)
-    
+    SelectedRandom = Math.floor(Math.random() * 100)
+    numberOfTries = 0
+    document.getElementById("makesNewText").innerHTML = "restarted";
+    document.getElementById("amountOfTries").innerHTML = "Tries : 0";
 }
 
 //checks and answers to the user
 function Checkifnumber() {
-    
-        
         let userInput = document.getElementById("UserText").value
-        
         document.getElementById("makesNewText").innerHTML = SelectedRandom.toString();
         
         if(Number(userInput) > SelectedRandom){
@@ -20,9 +21,9 @@ function Checkifnumber() {
         } else if(Number(userInput) == SelectedRandom){
             document.getElementById("makesNewText").innerHTML = "you guessed it, it was " + userInput.toString();
         } else {
-           //document.getElementById("makesNewText").innerHTML = "Do you even know what a number is?";
+           document.getElementById("makesNewText").innerHTML = "Do you even know what a number is?";
         }
-        
-    }
-
-
+    
+        numberOfTries++
+        document.getElementById("amountOfTries").innerHTML = "Tries : " + numberOfTries.toString();
+}
